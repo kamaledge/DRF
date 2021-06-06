8146d571899a4b0d8aac8ead8a231b90bcdccb3b
kamal <kamalnathtiwari89@gmail.com>
2021-06-06 22:10:54 +0530

    map application added

---
+++ drf/ApiConsume/map/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+url = https://positioning.hereapi.com/v2
+
+# https://developer.here.com/documentation/positioning-api/dev_guide/topics/construct-locate-request.html
